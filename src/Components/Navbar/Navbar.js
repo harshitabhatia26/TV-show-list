19aae6bca0efc6de64cdf5f1637b8a89dcb704b6
harshitabhatia26 <bharshita2604@gmail.com>
2023-08-08 15:09:55 +0530

    Responsivene design added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 
 function Navbar() {
     return(
-        <div className="navbar container-fluid navbar-light bg-light border-bottom p-4 " >
+        <div className="navbar navbar-light bg-light border-bottom p-4 " >
             <img className="navbar-brand"src={require('./QBT Logo Black.png')} alt="airbnb logo" width="100vw"></img>
             <ul className="navbar-nav ml-auto d-none d-md-flex flex-row justify-content-right">
             <li><a class="nav-link" href="#">Headlines</a></li>
