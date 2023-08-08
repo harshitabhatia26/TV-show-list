a8d76b903a962b615a3aa757efc90d134b82991d
harshitabhatia26 <bharshita2604@gmail.com>
2023-08-08 14:27:49 +0530

    Responsive design

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 
 function Navbar() {
     return(
-        <div className="navbar navbar-expand-lg navbar-light bg-light border-bottom p-4 " >
+        <div className="navbar container-fluid navbar-light bg-light border-bottom p-4 " >
             <img className="navbar-brand"src={require('./QBT Logo Black.png')} alt="airbnb logo" width="100vw"></img>
-            <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav ml-auto d-none d-md-flex flex-row justify-content-right">
             <li><a class="nav-link" href="#">Headlines</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Articles</a></li>
-					<li class="nav-item active"><a class="nav-link" href="#">Movies & Films</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Television</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Music</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Celebrities</a></li>
+					<li class="nav-item px-2"><a class="nav-link" href="#">Articles</a></li>
+					<li class="nav-item active px-2"><a class="nav-link" href="#">Movies & Films</a></li>
+					<li class="nav-item px-2"><a class="nav-link" href="#">Television</a></li>
+					<li class="nav-item px-2"><a class="nav-link" href="#">Music</a></li>
+					<li class="nav-item px-2"><a class="nav-link" href="#">Celebrities</a></li>
 				</ul>
         </div>
     )
